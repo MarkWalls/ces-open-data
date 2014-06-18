1135de08d6cab1f61d266a3ed80347554df4a084
Mark Walls <dwalls724@gmail.com>
2014-06-18 05:53:44 -0700

    Added stubs for four API routes to search data
    Added four routes:
    1) /api/:version/search/zip/:zip - etting the information about a
    zipcode as of the most recent date we have
    2) /api/:version/search/zip/:zip/daterange - getting the information
    about a zipcode within a daterange
    3) /api/:version/search/zip/:zipA/vs/:zipB - etting a comparison between
    two zip codes using the most up to date info
    4) /api/:version/search/zip/:zipA/vs/:zipB/daterange - getting a
    comparison between two zip codes using a date range

--- app.rb
+++ app.rb
@@ -37,3 +37,30 @@ get '/api/:version/samples.?:format?' do
   @samples = Sample.all
   json @samples
 end
+
+
+#http://localhost:4567/api/v0/search/zip/97213
+get '/api/:version/search/zip/:zip' do
+	#For getting the information about a zipcode as of the most recent date we have					
+		return "Zip by closest date to current time: " + params[:zip] + " at " + DateTime.now.strftime('%a %d %b %Y') 
+end
+
+
+#http://localhost:4567/api/v0/search/zip/97213/daterange?datestart=%221-1-2014%22&dateend=%222-1-2014%22
+get '/api/:version/search/zip/:zip/daterange' do
+	#For getting the information about a zipcode within a daterange					
+		return "Zip by date range: " + DateTime.parse(params[:datestart]).strftime('%a %d %b %Y')  + " to " + DateTime.parse(params[:dateend]).strftime('%a %d %b %Y') 
+end
+
+#http://localhost:4567/api/v0/search/zip/97213/vs/97045
+get '/api/:version/search/zip/:zipA/vs/:zipB' do
+	#For getting a comparison between two zip codes using the most up to date info
+	return "ZipCode " + params[:zipA] + " vs " + params[:zipB]
+end
+
+#http://localhost:4567/api/v0/search/zip/97213/vs/97045/daterange?datestart=%221-1-2014%22&dateend=%222-1-2014%22
+get '/api/:version/search/zip/:zipA/vs/:zipB/daterange' do
+	#For getting a comparison between two zip codes using a date range
+	return "ZipCode " + params[:zipA] + " vs " + params[:zipB] + " in daterange" + DateTime.parse(params[:datestart]).strftime('%a %d %b %Y')  + " to " + DateTime.parse(params[:dateend]).strftime('%a %d %b %Y') 
+end
+
